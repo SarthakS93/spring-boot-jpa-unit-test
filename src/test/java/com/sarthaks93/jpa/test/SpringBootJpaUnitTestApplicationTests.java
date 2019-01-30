a38d695f007416a8df0193d2841c500aee0cef04
sarthak <sarthaks93@gmail.com>
2019-01-31 01:13:58 +0530

    init commit

---
+++ src/test/java/com/sarthaks93/jpa/test/SpringBootJpaUnitTestApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sarthaks93.jpa.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootJpaUnitTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
