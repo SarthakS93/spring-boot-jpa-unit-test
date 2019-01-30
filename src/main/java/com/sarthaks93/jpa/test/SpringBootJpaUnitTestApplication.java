9b9c8c32de90ad47c34e56e3b23a43694a6da83e
sarthak <sarthaks93@gmail.com>
2019-01-31 01:14:44 +0530

    initial test class

---
+++ src/main/java/com/sarthaks93/jpa/test/SpringBootJpaUnitTestApplication.java
@@ -0,0 +1,14 @@
+package com.sarthaks93.jpa.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaUnitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaUnitTestApplication.class, args);
+	}
+
+}
+
